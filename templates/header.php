c7fdc6f373613e509c45a8ebd9020ed32fa9e6c5
Elle <eleonormcdowell@gmail.com>
2021-01-22 13:07:06 +0100

    Small changes on basic navbar

--- templates/header.php
+++ templates/header.php
@@ -2,20 +2,51 @@
 
 # Start session because this will be on every page.
 
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?> </title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/minty/bootstrap.min.css">
     <!-- My CSS -->
-    <link rel="stylesheet" href="style.css">
-    
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
+
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand mr-auto" href="#">Mini-SM</a>
+  
+  <!-- If you're on a small screen the menu will collapse -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Users</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Edit my profile</a>
+      </li>
+      
+    </ul>
+
+    <a href="/logout.php">
+        <button type="button" class="btn btn-secondary my-2 my-sm-0">Log Out</button>
+    </a>
+  
+  </div>
+</nav>
