f5fa1280e4694d29afabcbe0bb990393d9928a0e
Elle <eleonormcdowell@gmail.com>
2021-01-22 12:36:30 +0100

    basic files

---
+++ templates/header.php
@@ -0,0 +1,21 @@
+<?php
+
+# Start session because this will be on every page.
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?> </title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
