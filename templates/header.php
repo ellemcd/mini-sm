fe4cbe7cfb5c4e4b04c4b5b8615f9d28193cf440
Elle <eleonormcdowell@gmail.com>
2021-01-22 13:56:04 +0100

    login page style and html

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,7 @@
 <?php
 
 # Start session because this will be on every page.
+require ('functions.php');
 
 ?>
 
@@ -17,13 +18,16 @@
     <!-- My CSS -->
     <link rel="stylesheet" href="css/style.css">
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
 </head>
 
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand mr-auto" href="#">Mini-SM</a>
-  
+
   <!-- If you're on a small screen the menu will collapse -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
